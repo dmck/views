5efc29b212c248196c5edd0a600972f154a90904
dmck <dmck@users.noreply.github.com>
2016-03-05 13:46:05 -0500

    Get bootstrap and jekyll to play nice

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+grunt.initConfig({
+	// compile 'bootrap'
+    hub: {
+      all: {
+        src: ['bootstrap/Gruntfile.js'],
+        tasks: ['default'],
+      },
+    },
+    // copy compiled 'bootstrap' to 'jekyll-site'
+    copy: {
+      bootstrap: {
+		expand: true,
+		cwd: 'bootstrap/dist',
+		src: '**',
+		dest: 'jekyll-site/',
+	  },
+    },
+    // compile jekyll-site to 'dist'
+    shell: {
+        target: {
+            command: 'jekyll build -s, --source jekyll-site -d dist',
+        },
+    },
+
+});
+
+  grunt.loadNpmTasks('grunt-hub');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-shell');
+
+  // compile bootstrap, copy, compile jekyll
+  grunt.registerTask('default', ['hub', 'copy:bootstrap', 'shell']);
+
+};	
